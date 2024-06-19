b275106c80a528af89e8b57a3340f629be06d39d
Jeremy <jerycohe05@gmail.com>
2024-06-19 15:07:57 -0500

    Config

---
+++ src/db/config.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+});
+
+pool.on('connect', () => {
+    console.log('Connected to the database');
+});
+
+pool.on('error', (err) => {
+    console.error('Error connecting to the database:', err);
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+};
