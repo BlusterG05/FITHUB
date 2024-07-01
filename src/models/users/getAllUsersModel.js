8c2d22a3b520e2acb69057c18e8053ce11d16343
Jeremy <jerycohe05@gmail.com>
2024-06-30 20:43:05 -0500

    Models update

--- src/models/users/getAllUsersModel.js
+++ src/models/users/getAllUsersModel.js
@@ -6,6 +6,7 @@ const getAllUsersModel = async () => {
     `;
     try {
         const res = await query(sql);
+        console.log("Query complete")
         return res.rows;
     } catch (err) {
         throw new Error('Error fetching all users: ' + err.message);
