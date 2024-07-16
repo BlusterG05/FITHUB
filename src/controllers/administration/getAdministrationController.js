21ca4a2fc7408d7514a767d63a4200c7839866d7
BlusterG05 <jerycohe05@gmail.com>
2024-07-15 23:36:31 -0500

    'Login_JWT'

--- src/controllers/administration/getAdministrationController.js
+++ src/controllers/administration/getAdministrationController.js
@@ -1,5 +1,8 @@
-
+const bcrypt = require('bcrypt');
 const getAdministrationUserService = require('../../services/Administration/getAdministrationService');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 
 const getAdministrationLoginController = async (req, res) => {
     const { Mail, Password } = req.body;
@@ -7,14 +10,24 @@ const getAdministrationLoginController = async (req, res) => {
     try {
         const adminUser = await getAdministrationUserService(Mail);
         if (adminUser && adminUser.length > 0) {
-            //console.log("Username: " + adminUser[0].admin_username);
-            res.status(200).send("Username: " + adminUser[0].admin_username);
+            const hashedPassword = adminUser[0].admin_password; 
+            const match = await bcrypt.compare(Password, hashedPassword);
+            if (match) {
+                const token = jwt.sign(
+                    { id: adminUser[0].admin_id, username: adminUser[0].admin_username },
+                    process.env.JWT_SECRET,
+                    { expiresIn: '1h' }
+                );
+
+                res.status(200).json({ message:"Login succesfull!", token });
+            } else {
+                res.status(401).send('Invalid Password');
+            }
         } else {
-            //console.log('No user found');
-            res.status(401).send('User dont found');
+            res.status(401).send('User not found');
         }
     } catch (err) {
-        console.error('User Doesnt exist: ' + err.message);
+        console.error('Error during login:', err);
         res.status(500).send('Internal Server Error');
     }
 };
