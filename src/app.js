bc672c3424dccc8c05c25e1ce75f5e788a77a31a
BlusterG05 <jerycohe05@gmail.com>
2024-07-20 00:27:16 -0500

    'Oauth_feature'

--- src/app.js
+++ src/app.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const { generateJWT } = require('./utils/jwtUtils');
+const passport = require('./config/passport'); // Asegúrate de que este archivo exista
+const cors = require('cors');
 const usersRoutes = require('./routes/users');
-const trainersRoutes = require('./routes/trainers');  // Actualización aquí
+const trainersRoutes = require('./routes/trainers');
 const machinesRoutes = require('./routes/machines');
 const exercisesRoutes = require('./routes/exercises');
 const administrationRoutes = require('./routes/administrationUser');
-const authenticateUser = require ('./middlewares/authenticateUser')
+const authenticateUser = require('./middlewares/authenticateUser');
 
-const app = express();
+const app = express(); // Inicializa la aplicación Express
 
+app.use(cors());
 app.use(bodyParser.json());
+app.use(session({ 
+  secret: process.env.SESSION_SECRET, 
+  resave: false, 
+  saveUninitialized: true 
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use('/api/users', authenticateUser, usersRoutes);
+app.use('/api/trainers', authenticateUser, trainersRoutes);
+app.use('/api/machines', authenticateUser, machinesRoutes);
+app.use('/api/exercises', authenticateUser, exercisesRoutes);
+app.use('/api/admin', administrationRoutes);
 
-app.use('/api/users',authenticateUser, usersRoutes);
-app.use('/api/trainers',authenticateUser, trainersRoutes);
-app.use('/api/machines',authenticateUser, machinesRoutes);
-app.use('/api/exercises',authenticateUser, exercisesRoutes);
-app.use('/api/admin',administrationRoutes);
+// Rutas OAuth
+app.get('/auth/oauth', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get('/auth/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    const token = generateJWT(req.user);
+    res.redirect(`http://localhost:5173/auth/callback?token=${token}`);
+  }
+);
 
 if (require.main === module) {
   app.listen(3000, () => {
@@ -25,4 +46,3 @@ if (require.main === module) {
 }
 
 module.exports = app;
-
